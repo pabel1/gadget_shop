d88436d476399b368411135849e523487d17019b
pabel1 <pabelahamed39@gmail.com>
2023-10-20 21:55:47 +0600

    subcategories created route define with validation schema

--- src/app/modules/SubCategory/subCategories.router.js
+++ src/app/modules/SubCategory/subCategories.router.js
@@ -1,14 +1,20 @@
 /* eslint-disable node/no-extraneous-require */
 const express = require("express");
+const FileUploadHelper = require("../../../Middleware/uploadMiddleware");
+const validateRequest = require("../../../Middleware/validateRequest");
+const JoiSubCategoriesValidationSchema = require("./subCategories.validation");
+const subCategoriesController = require("./subCategories.controller");
 
 const router = express.Router();
 
-// router.post(
-//   "/create",
-//   FileUploadHelper.upload.single("image"),
-//   validateRequest(JoiCategoriesValidationSchema.categoriesValidationSchema),
-//   categoriesController.createCategories
-// );
+router.post(
+  "/create",
+  FileUploadHelper.upload.single("image"),
+  validateRequest(
+    JoiSubCategoriesValidationSchema.createSubCategoriesValidationSchema
+  ),
+  subCategoriesController.createSubCategories
+);
 
 const subCategoriesRouter = router;
 
