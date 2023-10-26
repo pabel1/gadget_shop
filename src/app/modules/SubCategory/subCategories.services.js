c9d359f4ba324fac8e2c600917f1c281bd48c83b
pabel1 <pabelahamed39@gmail.com>
2023-10-26 23:50:17 +0600

    get all subCategory adde using dynamic filtering sorting pagination ans search

--- src/app/modules/SubCategory/subCategories.services.js
+++ src/app/modules/SubCategory/subCategories.services.js
@@ -1,6 +1,11 @@
 const httpStatus = require("http-status");
 const ErrorHandler = require("../../../ErrorHandler/errorHandler");
 const SubcategoriesModel = require("./subCategories.model");
+const { paginationHelpers } = require("../../../Helper/paginationHelper");
+const { subCategorySearchableFields } = require("./subCategories.constant");
+const { searchHelper } = require("../../../Helper/searchHelper");
+const { filteringHelper } = require("../../../Helper/filteringHelper");
+const { sortingHelper } = require("../../../Helper/sortingHelper");
 
 const createSubCategoriesIntoDB = async (payload) => {
   const isExist = await SubcategoriesModel.findOne({
@@ -17,21 +22,71 @@ const createSubCategoriesIntoDB = async (payload) => {
 
   return newSubCategory;
 };
-const getAllSubCategoryFromDB = async (payload) => {
-  const isExist = await SubcategoriesModel.findOne({
-    subcategoryName: payload?.subcategoryName,
-  });
-  if (isExist) {
-    throw new ErrorHandler(
-      `${isExist.subcategoryName} this Subcategory already exist!`,
-      httpStatus.CONFLICT
-    );
-  }
-  const subCategories = new SubcategoriesModel(payload);
-  const newSubCategory = await subCategories.save();
+const getAllSubCategoryFromDB = async (filters, paginationOptions) => {
+  const { searchTerm, ...filtersData } = filters;
 
-  return newSubCategory;
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions);
+
+  const pipeline = [];
+  const totalPipeline = [];
+  const matchAnd = [];
+
+  //?Dynamic search added
+  const dynamicSearchQuery = searchHelper.createSearchQuery(
+    searchTerm,
+    subCategorySearchableFields
+  );
+  if (dynamicSearchQuery) {
+    matchAnd.push(dynamicSearchQuery);
+  }
+  // ? Dynamic filtering added
+  const dynamicFilter = filteringHelper.createDynamicFilter(filtersData);
+
+  if (dynamicFilter) {
+    matchAnd.push(dynamicFilter);
+  }
+  if (skip) {
+    pipeline.push({ $skip: skip });
+  }
+
+  if (limit) {
+    pipeline.push({ $limit: limit });
+  }
+
+  // sorting
+
+  const dynamicSorting = sortingHelper.createDynamicSorting(sortBy, sortOrder);
+  console.log(dynamicSorting);
+  if (dynamicSorting) {
+    pipeline.push({
+      $sort: dynamicSorting.sortObject,
+    });
+  }
+
+  // if join projection and otherneeded for before match ar unshift then write here
+
+  if (matchAnd.length) {
+    pipeline.unshift({
+      $match: { $and: matchAnd },
+    });
+    totalPipeline.unshift({
+      $match: { $and: matchAnd },
+    });
+  }
+  const result = await SubcategoriesModel.aggregate(pipeline);
+  const total = await SubcategoriesModel.aggregate(totalPipeline);
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
 };
+
 const SubCategoriesServices = {
   createSubCategoriesIntoDB,
   getAllSubCategoryFromDB,
