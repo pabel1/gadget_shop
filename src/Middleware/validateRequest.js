235d489705d4c65aa1586a18b4d9829ca2b2bdf7
pabel1 <pabelahamed39@gmail.com>
2023-10-27 21:51:58 +0600

    category created

--- src/Middleware/validateRequest.js
+++ src/Middleware/validateRequest.js
@@ -1,5 +1,8 @@
 const validateRequest = (schema) => async (req, res, next) => {
   try {
+    if (req.body.subCategory) {
+      req.body.subCategory = JSON.parse(req.body.subCategory);
+    }
     await schema.validateAsync(req.body);
 
     return next();
