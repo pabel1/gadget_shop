183b57007a54051aa775258dbdbb1744990dd739
pabel1 <pabelahamed39@gmail.com>
2023-12-24 23:20:21 +0600

    tag create done

--- src/app/modules/Tag/tag.controller.js
+++ src/app/modules/Tag/tag.controller.js
@@ -7,6 +7,26 @@ const sendResponse = require("../../../shared/sendResponse");
 const tagServices = require("./tag.services");
 const tagConstant = require("./tag.constant");
 
+const createIndividualTag = catchAsyncError(async (req, res) => {
+  const { tagFor, tagTitle } = req.body;
+  //   generate compositeKey
+  const compositeKey = compositeKeyGenerator.generateCompositKey({
+    keyFor: tagConstant?.compositeKeyFor[0] || "tag",
+    tagFor,
+    tagTitle,
+  });
+  req.body.tagCompositKey = compositeKey;
+  const result = await tagServices.createTagIntoDB(req.body);
+
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: "Tag created successfully",
+    data: {
+      result,
+    },
+  });
+});
 const createTag = catchAsyncError(async (req, res) => {
   const { tagFor, tagTitle } = req.body;
   //   generate compositeKey
@@ -30,5 +50,6 @@ const createTag = catchAsyncError(async (req, res) => {
 
 const tagController = {
   createTag,
+  createIndividualTag,
 };
 module.exports = tagController;
