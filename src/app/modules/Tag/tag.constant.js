a63c5ce939a40cb4f86abe5bc902d23e041f88cd
pabel1 <pabelahamed39@gmail.com>
2023-12-30 00:00:20 +0600

    tag constant some added

--- src/app/modules/Tag/tag.constant.js
+++ src/app/modules/Tag/tag.constant.js
@@ -1,7 +1,24 @@
 const compositeKeyFor = ["tag"];
 
-const tagConstant = {
+const tagCompositConstant = {
   compositeKeyFor,
 };
 
+const tagsFilterableFields = [
+  "categoryName",
+  "categoryTag",
+  "slug",
+  "searchTerm",
+  "discount",
+  "_id",
+];
+
+const tagsSearchableFields = ["categoryName", "categoryTag", "slug"];
+
+module.exports = tagConstant;
+const tagConstant = {
+  tagCompositConstant,
+  tagsFilterableFields,
+  tagsSearchableFields,
+};
 module.exports = tagConstant;
